788965d003d9114ae42a4372b7b16d6749456789
cheese7 <46264823+cheese7@users.noreply.github.com>
2020-02-15 12:08:15 +0000

    Add my own solution to 5.5 Putting it all into practice

--- src/main/java/lambdasinaction/chap5/PuttingIntoPractice.java
+++ src/main/java/lambdasinaction/chap5/PuttingIntoPractice.java
@@ -3,13 +3,56 @@ package lambdasinaction.chap5;
 import lambdasinaction.chap5.*;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.Comparator.comparing;
 import static java.util.stream.Collectors.toList;
 
 public class PuttingIntoPractice{
     public static void main(String ...args){    
-        Trader raoul = new Trader("Raoul", "Cambridge");
+        List<Transaction> transactions = initTransactions();	
+        List<Transaction> s1 = transactions.stream().filter(i -> i.getYear()==2011).sorted(Comparator.comparing(Transaction::getValue)).collect(Collectors.toList());
+//        s1.forEach(System.out::println);
+        System.out.println(s1);
+        
+        List<String> s2 = transactions.stream().map(i -> i.getTrader().getCity()).distinct().collect(Collectors.toList());
+//        s2.forEach(System.out::println);
+        System.out.println(s2);
+        
+        Set<String> s2Set = transactions.stream().map(i -> i.getTrader().getCity()).collect(Collectors.toSet());
+//        s2Set.forEach(System.out::println);
+        System.out.println(s2Set);
+        
+        List<Trader> s3 = transactions.stream().map(Transaction::getTrader)
+        		.filter(i -> "Cambridge".equals(i.getCity())).distinct().sorted(Comparator.comparing(Trader::getName)).collect(Collectors.toList());
+//        s3.forEach(System.out::println);
+        System.out.println(s3);
+        
+        String s4 = transactions.stream().map(i -> i.getTrader().getName()).distinct().sorted().collect(Collectors.joining(", "));
+        System.out.println(s4);
+        
+        boolean s5 = transactions.stream().anyMatch(i -> "Milan".equals(i.getTrader().getCity()));
+        System.out.println(s5);
+        
+        // s6
+        transactions.stream().filter(i -> "Cambridge".equals(i.getTrader().getCity())).map(Transaction::getValue).forEach(System.out::println);
+        
+        Optional<Integer> s7 = transactions.stream().map(Transaction::getValue).reduce(Integer::max);
+        System.out.println(s7);
+        
+        Optional<Integer> s8 = transactions.parallelStream().map(Transaction::getValue).reduce(Integer::min);
+        System.out.println(s8);
+        
+        // official solutions. NB: Q6 and Q8 seem to be different from the book. 
+        solve(transactions);      
+    }
+    
+
+	/**
+	 * @return
+	 */
+	private static List<Transaction> initTransactions() {
+		Trader raoul = new Trader("Raoul", "Cambridge");
         Trader mario = new Trader("Mario","Milan");
         Trader alan = new Trader("Alan","Cambridge");
         Trader brian = new Trader("Brian","Cambridge");
@@ -21,10 +64,15 @@ public class PuttingIntoPractice{
             new Transaction(mario, 2012, 710),	
             new Transaction(mario, 2012, 700),
             new Transaction(alan, 2012, 950)
-        );	
-        
-        
-        // Query 1: Find all transactions from year 2011 and sort them by value (small to high).
+        );
+		return transactions;
+	}
+
+	/**
+	 * @param transactions
+	 */
+	private static void solve(List<Transaction> transactions) {
+		// Query 1: Find all transactions from year 2011 and sort them by value (small to high).
         List<Transaction> tr2011 = transactions.stream()
                                                .filter(transaction -> transaction.getYear() == 2011)
                                                .sorted(comparing(Transaction::getValue))
@@ -85,6 +133,7 @@ public class PuttingIntoPractice{
             transactions.stream()
                         .map(Transaction::getValue)
                         .reduce(0, Integer::max);
-        System.out.println(highestValue);      
-    }
+        System.out.println(highestValue);
+	}
+
 }
